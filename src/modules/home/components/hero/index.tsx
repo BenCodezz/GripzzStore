d07c501886f0e9f9ec18f37719d2b0df0ad742cf
BenGripzz <benbleiker08@gmail.com>
2023-05-04 14:00:40 +0200

    #2

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -5,12 +5,11 @@ const Hero = () => {
   return (
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
-        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here
+        <h1 className="text-Summer2xl-semi mb-4 drop-shadow-md shadow-black">
+           Wellcome to Gripzz
         </h1>
         <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+          Uprade your Ride ASAP!
         </p>
         <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>
